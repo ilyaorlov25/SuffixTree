bcfd384f83cf61a627156c93224dbd38fd595403
ilyaorlov25 <ilya.orlov2002@yandex.ru>
2020-11-25 01:58:35 +0300

    некоторые шаблоны

--- src/main/java/SuffixTree.java
+++ src/main/java/SuffixTree.java
@@ -1,2 +1,55 @@
+// Source: https://www.youtube.com/watch?v=kxqlaynGgEA & https://habr.com/ru/post/258121/
+
+import java.util.Map;
+
 public class SuffixTree {
+    private class Node {
+        int edgeStart;
+        int edgeLength;
+        Map<Character, Node> children;
+        Node parent;
+        Map<Character, Node> prefixLink;
+
+        // for usual nodes
+        public Node(int edgeStart, int edgeLength, Node parent) {
+            this.edgeStart = edgeStart;
+            this.edgeLength = edgeLength;
+            this.parent = parent;
+        }
+
+        // for root & fictive
+        public Node() {
+        }
+    }
+
+    private final Node fictive = new Node();
+    private final Node root;
+    private int size;
+    private int stringLength;
+
+    public SuffixTree(String string) {
+        /*
+        '$' (не встречающийся больше символ) показывает, где заканчивается суффикс. Благодаря ему становится верным
+        утверждение, что каждый суффикс - это лист, а каждый лист - это суффикс
+         */
+
+        String in = string + "$";
+        stringLength = in.length();
+        size = 0;
+
+
+        root = new Node();
+        root.parent = fictive;
+    }
+
+    private void extend() {
+
+    }
+
+    private boolean insert(Character symbol) {
+        boolean inserted = false;
+        // TODO
+        size++;
+        return inserted;
+    }
 }
